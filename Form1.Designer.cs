d981f6797409a0edd5d87823b86b1bd96cae02b4
Jakub Michalski <jmich2002@outlook.com>
2024-05-17 05:16:47 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace UpdaterMain
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            arr5 = new Label();
+            arr4 = new Label();
+            arr3 = new Label();
+            arr2 = new Label();
+            stat2 = new Label();
+            stat5 = new Label();
+            stat4 = new Label();
+            stat3 = new Label();
+            stat1 = new Label();
+            arr1 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            downStatus = new Label();
+            progressBar1 = new ProgressBar();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(arr5);
+            groupBox1.Controls.Add(arr4);
+            groupBox1.Controls.Add(arr3);
+            groupBox1.Controls.Add(arr2);
+            groupBox1.Controls.Add(stat2);
+            groupBox1.Controls.Add(stat5);
+            groupBox1.Controls.Add(stat4);
+            groupBox1.Controls.Add(stat3);
+            groupBox1.Controls.Add(stat1);
+            groupBox1.Controls.Add(arr1);
+            groupBox1.Location = new Point(12, 31);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(300, 212);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // arr5
+            // 
+            arr5.Enabled = false;
+            arr5.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            arr5.Location = new Point(16, 160);
+            arr5.Name = "arr5";
+            arr5.Size = new Size(35, 29);
+            arr5.TabIndex = 16;
+            arr5.Text = "→";
+            arr5.Visible = false;
+            // 
+            // arr4
+            // 
+            arr4.Enabled = false;
+            arr4.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            arr4.Location = new Point(16, 125);
+            arr4.Name = "arr4";
+            arr4.Size = new Size(35, 29);
+            arr4.TabIndex = 15;
+            arr4.Text = "→";
+            arr4.Visible = false;
+            // 
+            // arr3
+            // 
+            arr3.Enabled = false;
+            arr3.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            arr3.Location = new Point(16, 91);
+            arr3.Name = "arr3";
+            arr3.Size = new Size(35, 29);
+            arr3.TabIndex = 14;
+            arr3.Text = "→";
+            arr3.Visible = false;
+            // 
+            // arr2
+            // 
+            arr2.Enabled = false;
+            arr2.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            arr2.Location = new Point(16, 56);
+            arr2.Name = "arr2";
+            arr2.Size = new Size(35, 29);
+            arr2.TabIndex = 13;
+            arr2.Text = "→";
+            arr2.Visible = false;
+            // 
+            // stat2
+            // 
+            stat2.AutoSize = true;
+            stat2.Enabled = false;
+            stat2.Location = new Point(53, 67);
+            stat2.Name = "stat2";
+            stat2.Size = new Size(187, 15);
+            stat2.TabIndex = 12;
+            stat2.Text = "Łączenie z serwerem aktualizacji ...";
+            // 
+            // stat5
+            // 
+            stat5.AutoSize = true;
+            stat5.Enabled = false;
+            stat5.Location = new Point(53, 172);
+            stat5.Name = "stat5";
+            stat5.Size = new Size(146, 15);
+            stat5.TabIndex = 11;
+            stat5.Text = "Instalowanie aktualizacji ...";
+            // 
+            // stat4
+            // 
+            stat4.AutoSize = true;
+            stat4.Enabled = false;
+            stat4.Location = new Point(53, 137);
+            stat4.Name = "stat4";
+            stat4.Size = new Size(136, 15);
+            stat4.TabIndex = 10;
+            stat4.Text = "Pobieranie aktualizacji ...";
+            // 
+            // stat3
+            // 
+            stat3.AutoSize = true;
+            stat3.Enabled = false;
+            stat3.Location = new Point(53, 102);
+            stat3.Name = "stat3";
+            stat3.Size = new Size(211, 15);
+            stat3.TabIndex = 9;
+            stat3.Text = "Sprawdzanie dostępnych aktualizacji ...";
+            // 
+            // stat1
+            // 
+            stat1.AutoSize = true;
+            stat1.Location = new Point(53, 33);
+            stat1.Name = "stat1";
+            stat1.Size = new Size(224, 15);
+            stat1.TabIndex = 8;
+            stat1.Text = "Sprawdzanie połączenia internetowego ...";
+            // 
+            // arr1
+            // 
+            arr1.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            arr1.Location = new Point(16, 21);
+            arr1.Name = "arr1";
+            arr1.Size = new Size(35, 29);
+            arr1.TabIndex = 3;
+            arr1.Text = "→";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(275, 19);
+            label1.TabIndex = 1;
+            label1.Text = "Sprawdzanie aktualizacji programu ...";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.updater;
+            pictureBox1.Location = new Point(12, 249);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(300, 78);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // downStatus
+            // 
+            downStatus.AutoSize = true;
+            downStatus.Location = new Point(12, 341);
+            downStatus.Name = "downStatus";
+            downStatus.Size = new Size(72, 15);
+            downStatus.TabIndex = 13;
+            downStatus.Text = "Pobieranie...";
+            downStatus.Visible = false;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 366);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(300, 23);
+            progressBar1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(324, 401);
+            ControlBox = false;
+            Controls.Add(progressBar1);
+            Controls.Add(downStatus);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MaximumSize = new Size(340, 440);
+            MinimizeBox = false;
+            MinimumSize = new Size(340, 440);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Proszę czekać ...";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label arr5;
+        private Label arr4;
+        private Label arr3;
+        private Label arr2;
+        private Label stat2;
+        private Label stat5;
+        private Label stat4;
+        private Label stat3;
+        private Label stat1;
+        private Label arr1;
+        private Label downStatus;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,425 @@
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Net.NetworkInformation;
+using System.Net;
+using System;
+using System.Windows.Forms;
+
+namespace UpdaterMain
+{
+    public partial class Form1 : Form
+    {
+        int langSel;
+        int languageSelected;
+        string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "./data/config.ini");
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadConfig();
+            CheckConnection();
+        }
+
+        private void LoadConfig()
+        {
+            if (File.Exists(configPath))
+            {
+                string[] lines = File.ReadAllLines(configPath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split('=');
+                    if (parts.Length == 2)
+                    {
+                        switch (parts[0])
+                        {
+                            case "langSel":
+                                langSel = int.Parse(parts[1]);
+                                break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Missing config data or language file corrupted...", "Error", MessageBoxButtons.OK);
+            }
+            if (langSel == 1)
+            {
+                languageSelected = 1;
+
+            }
+            if (langSel == 0)
+            {
+                languageSelected = 0;
+            }
+        }
+
+
+
+        private async Task<bool> CheckServerAvailability(string serverAddress)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply reply = await ping.SendPingAsync(serverAddress, 5000);
+
+                return reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                return false;
+            }
+        }
+
+        private async void CheckConnection()
+        {
+            await Task.Delay(3000);
+
+            if (await CheckServerAvailability("windowsbase.pl"))
+            {
+                arr1.Visible = true;
+                arr1.Enabled = true;
+                stat1.Enabled = true;
+                stat1.Visible = true;
+
+                arr2.Visible = false;
+                arr2.Enabled = false;
+                stat2.Enabled = false;
+
+                arr3.Visible = false;
+                arr3.Enabled = false;
+                stat3.Enabled = false;
+
+                arr4.Visible = false;
+                arr4.Enabled = false;
+                stat4.Enabled = false;
+
+                arr5.Enabled = false;
+                arr5.Visible = false;
+                stat5.Enabled = false;
+
+
+                runUpdate();
+            }
+            else
+            {
+                if (languageSelected == 1)
+                {
+                    MessageBox.Show("There was a problem while connecting to the update server. Make sure you have a working Internet connection or contact technical support available at: www.windowsbase.pl", "Connection error", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Wyst¹pi³ problem podczas po³¹czenia z serwerem aktualizacji. Upewnij siê, czy masz dzia³aj¹ce po³¹czenie z Internetem lub skontaktuj siê z pomoc¹ techniczn¹ dostêpn¹ pod adresem: www.windowsbase.pl", "B³¹d po³¹czenia", MessageBoxButtons.OK);
+                }
+
+                this.Close();
+            }
+        }
+
+        private void step2Anim()
+        {
+            arr2.Visible = true;
+            arr2.Enabled = true;
+            stat2.Enabled = true;
+            stat2.Visible = true;
+
+            arr1.Visible = false;
+            arr1.Enabled = false;
+            stat1.Enabled = false;
+
+            arr3.Visible = false;
+            arr3.Enabled = false;
+            stat3.Enabled = false;
+
+            arr4.Visible = false;
+            arr4.Enabled = false;
+            stat4.Enabled = false;
+
+            arr5.Enabled = false;
+            arr5.Visible = false;
+            stat5.Enabled = false;
+
+        }
+
+        private void step3Anim()
+        {
+            arr3.Visible = true;
+            arr3.Enabled = true;
+            stat3.Enabled = true;
+            stat3.Visible = true;
+
+            arr1.Visible = false;
+            arr1.Enabled = false;
+            stat1.Enabled = false;
+
+            arr2.Visible = false;
+            arr2.Enabled = false;
+            stat2.Enabled = false;
+
+            arr4.Visible = false;
+            arr4.Enabled = false;
+            stat4.Enabled = false;
+
+            arr5.Enabled = false;
+            arr5.Visible = false;
+            stat5.Enabled = false;
+
+        }
+
+        private void step4Anim()
+        {
+            arr4.Visible = true;
+            arr4.Enabled = true;
+            stat4.Enabled = true;
+            stat4.Visible = true;
+
+            arr1.Visible = false;
+            arr1.Enabled = false;
+            stat1.Enabled = false;
+
+            arr2.Visible = false;
+            arr2.Enabled = false;
+            stat2.Enabled = false;
+
+            arr3.Visible = false;
+            arr3.Enabled = false;
+            stat3.Enabled = false;
+
+            arr5.Enabled = false;
+            arr5.Visible = false;
+            stat5.Enabled = false;
+
+        }
+
+        private void step5Anim()
+        {
+            arr5.Visible = true;
+            arr5.Enabled = true;
+            stat5.Enabled = true;
+            stat5.Visible = true;
+
+            arr1.Visible = false;
+            arr1.Enabled = false;
+            stat1.Enabled = false;
+
+            arr2.Visible = false;
+            arr2.Enabled = false;
+            stat2.Enabled = false;
+
+            arr3.Visible = false;
+            arr3.Enabled = false;
+            stat3.Enabled = false;
+
+            arr4.Enabled = false;
+            arr4.Visible = false;
+            stat4.Enabled = false;
+
+        }
+
+
+
+        public async void runUpdate()
+        {
+            try
+            {
+                Process controler = new Process();
+                controler.StartInfo.FileName = Path.Combine(appDirectory, "./bin/pccontrol.bat");
+                controler.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                controler.StartInfo.CreateNoWindow = true;
+                controler.Start();
+
+
+                string updateDirectory = Path.Combine(appDirectory, "./data/update");
+                string localVersionFilePath = Path.Combine(updateDirectory, "version.txt");
+                if (!File.Exists(localVersionFilePath))
+                {
+                    if (languageSelected == 1)
+                    {
+                        controler.Kill();
+                        MessageBox.Show("An error occurred while checking for updates", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        controler.Kill();
+                        MessageBox.Show("Wyst¹pi³ b³¹d podczas sprawdzania aktualizacji", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    this.Close();
+                }
+
+                step2Anim();
+
+                string localVersion = File.ReadAllText(localVersionFilePath).Trim();
+                string remoteVersionFileUrl = "https://windowsbase.pl/uploads/apps/ydlp-gui/version.txt";
+                WebClient webClient = new();
+                string remoteVersionFileContent = webClient.DownloadString(remoteVersionFileUrl);
+                string[] remoteVersionLines = remoteVersionFileContent.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                string remoteVersion = "";
+                string updateUrl = "";
+
+                foreach (string line in remoteVersionLines)
+                {
+                    string[] parts = line.Split(new[] { ':' }, 2);
+                    if (parts.Length == 2)
+                    {
+                        remoteVersion = parts[0].Trim('"');
+                        updateUrl = parts[1].Trim('"');
+                    }
+                }
+
+                if (remoteVersion == null || updateUrl == null)
+                {
+                    if (languageSelected == 1)
+                    {
+                        controler.Kill();
+                        MessageBox.Show("An error occurred while checking for updates", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        controler.Kill();
+                        MessageBox.Show("Wyst¹pi³ b³¹d podczas sprawdzania aktualizacji", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    this.Close();
+                    return;
+                }
+
+                await Task.Delay(3000);
+                step3Anim();
+
+                if (localVersion == remoteVersion)
+                {
+                    if (languageSelected == 1)
+                    {
+                        controler.Kill();
+                        MessageBox.Show("No updates available, you have the latest version of the program: " + remoteVersion, "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        controler.Kill();
+                        MessageBox.Show("Brak dostêpnych aktualizacji, posiadasz najnowsz¹ wersjê programu: " + remoteVersion, "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    this.Close();
+                    return;
+                }
+                await Task.Delay(3000);
+                step4Anim();
+                progressBar1.Visible = true;
+                downStatus.Visible = true;
+                downStatus.Enabled = true;
+                if (languageSelected == 1)
+                {
+                    downStatus.Text = "Downloading file data...";
+                }
+                else
+                {
+                    downStatus.Text = "Pobieranie plików aktualizacji...";
+                }
+                progressBar1.Value = 33;
+                string tempDirectory = Path.GetTempPath();
+                string tempZipFilePath = Path.Combine(tempDirectory, "update.zip");
+                string tempExtractDirectory = Path.Combine(tempDirectory, "ytdlupdate");
+
+                Stopwatch stopwatch = new Stopwatch();
+
+                webClient.DownloadProgressChanged += (s, e) =>
+                {
+                    if (e.ProgressPercentage == 0)
+                        stopwatch.Start();
+                    else
+                    {
+                        double speed = e.BytesReceived / 1024d / 1024d / stopwatch.Elapsed.TotalSeconds;
+                        if (languageSelected == 1)
+                        {
+                            downStatus.Text = $"Downloading data: {e.BytesReceived / 1024.0 / 1024.0:0.00} MB / {e.TotalBytesToReceive / 1024.0 / 1024.0:0.00} MB @ {speed:0.00} MB/s";
+                        }
+                        else
+                        {
+                            downStatus.Text = $"Pobieranie danych: {e.BytesReceived / 1024.0 / 1024.0:0.00} MB / {e.TotalBytesToReceive / 1024.0 / 1024.0:0.00} MB @ {speed:0.00} MB/s";
+                        }
+                        progressBar1.Value = 33 + (int)(e.ProgressPercentage * 0.33);
+                    }
+                };
+
+                await webClient.DownloadFileTaskAsync(new Uri(updateUrl), tempZipFilePath);
+                stopwatch.Stop();
+
+                await Task.Delay(3000);
+                step5Anim();
+
+                if (Directory.Exists(tempExtractDirectory))
+                {
+                    Directory.Delete(tempExtractDirectory, true);
+                }
+
+                if (languageSelected == 1)
+                {
+                    downStatus.Text = "Instalacja aktualizacji...";
+                }
+                else
+                {
+                    downStatus.Text = "Installing updates...";
+                }
+
+                progressBar1.Value = 66;
+                await Task.Run(() => ZipFile.ExtractToDirectory(tempZipFilePath, tempExtractDirectory));
+
+                foreach (string file in Directory.GetFiles(tempExtractDirectory))
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destFilePath = Path.Combine(appDirectory, fileName);
+                    File.Copy(file, destFilePath, true);
+                }
+                if (languageSelected == 1)
+                {
+                    downStatus.Text = "Instalacja aktualizacji...";
+                }
+                else
+                {
+                    downStatus.Text = "Little housekeeping...";
+                }
+
+                progressBar1.Value = 100;
+                await Task.Delay(3000);
+                File.Delete(tempZipFilePath);
+
+                string exePath = Path.Combine(appDirectory, "ytdl-gui.exe");
+                Process.Start(exePath);
+
+                controler.Kill();
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                if (languageSelected == 1)
+                {
+                    controler.Kill();
+                    MessageBox.Show($"An error occured: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    controler.Kill();
+                    MessageBox.Show($"Wyst¹pi³ b³¹d: {ex.Message}", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                this.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (languageSelected == 1)
+            {
+                label1.Text = "Please wait";
+                stat1.Text = "Checking internet connection...";
+                stat2.Text = "Connecting to server...";
+                stat3.Text = "Checking for updates...";
+                stat4.Text = "Downloading updates...";
+                stat5.Text = "Installing updates...";
+            }
+            else
+            {
+               // do nothing ...
+            }
+
+        }
+    }
+}
